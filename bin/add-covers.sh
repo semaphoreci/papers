db26d31263e74d48272deb2ef84e0fc24574ea23
tomas f <46322567+TomFern@users.noreply.github.com>
2022-03-07 11:44:32 -0300

    test

--- bin/add-covers.sh
+++ bin/add-covers.sh
@@ -11,14 +11,16 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CPDF="${SCRIPT_DIR}/cpdf"
 TMP_DIR=$(mktemp -d)
 
-echo "script path is $SCRIPT_DIR"
-echo "temp dir is $TMP_DIR"
-
 for cover in "$COVER_DIR/"*.pdf; do
-    echo "Adding cover $cover"
     fn=$(basename "$cover")
-    $CPDF "$cover" "$SOURCE_DIR/$fn" -o "$TMP_DIR/$fn"
+    echo -n "Found cover $fn ..."
+    if [ -f "$SOURCE_DIR/$fn" ]; then
+        echo "adding cover to source PDF."
+        $CPDF "$cover" "$SOURCE_DIR/$fn" -o "$TMP_DIR/$fn"
+    else
+        echo "no source PDF found. Skipping this cover."
+    fi
 done
 
-mv "$TMP_DIR"/*.pdf "$SOURCE_DIR"
+mv -f "$TMP_DIR"/*.pdf "$SOURCE_DIR"
 rmdir "$TMP_DIR"
